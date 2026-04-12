7b4046b88abf0ecaadf6a6f42757dc5aeb6a31fe
Afag <afag.ramazanli01@gmail.com>
2026-04-12 22:00:48 +0400

    add vercel url to cors

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/debug', function () {
-    return response()->json(['status' => 'ok', 'env' => app()->environment()]);
-});
