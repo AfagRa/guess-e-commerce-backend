858a17d063b8d4581c3a0297e91d6c4dd382b9f3
Afag <afag.ramazanli01@gmail.com>
2026-04-12 14:41:47 +0400

    added debug route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/debug', function () {
+    try {
+        $count = \App\Models\Product::count();
+        return response()->json(['products' => $count, 'db' => 'connected']);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
+});
